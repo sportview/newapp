68d690735a09dbe278b305c4945c704548360826
sportview <kamauc63@gmail.com>
2019-01-11 13:20:43 +0300

    updated student.py

--- student.py
+++ student.py
@@ -4,7 +4,7 @@ class student:
         self.address=address
         self.email=email
         self.mobile=mobile
-        self._password=password
+        self.__password=password
     def displayname(self):
         print(self.sname)
     def setname(self,name):
@@ -12,8 +12,18 @@ class student:
     def getname(self):
         return self.sname
     def setpasword(self,password):
-        self._password=password
+        self.__password=password
     def getPassword(self):
-        return self._password   
+        return self.__password 
+
+    #protected tutorial  
+    #def setpasword(self,password):
+     #   self._password=password
+
+    #def getPassword(self):
+    #    return self._password   
+    
 
 hilder=student("hilder","P.O BOX 10202 NAIROBI","kamauc63@gmail.com","0706641899","Kingdom OF Jesus")
+hilder.displayname()
+print(hilder.getPassword())
